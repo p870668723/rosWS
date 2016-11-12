cb464531d0a0f338f55355fac867028ebd62eff6
= <=>
2016-11-13 01:41:01 +0800

    gmapping has been modified, it uses the max value of beam! Actually, it works better

---
+++ build/gmapping-ros/openslam_gmapping/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/pf/rosWS/src/gmapping-ros/openslam_gmapping
+# Build directory: /home/pf/rosWS/build/gmapping-ros/openslam_gmapping
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gridfastslam)
+SUBDIRS(scanmatcher)
+SUBDIRS(sensor)
+SUBDIRS(utils)
---
+++ build/gmapping-ros/openslam_gmapping/sensor/sensor_odometry/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/pf/rosWS/src/gmapping-ros/openslam_gmapping/sensor/sensor_odometry
+# Build directory: /home/pf/rosWS/build/gmapping-ros/openslam_gmapping/sensor/sensor_odometry
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
