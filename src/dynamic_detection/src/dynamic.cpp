4b9035c7df7af656eae08050136bafa6f1c8b4af
= <=>
2016-12-20 09:59:16 +0800

    解决floating point exception问题,系除数为0造成.接下来需要引入区域增长法,将各个动态物体分割开来,独立计算质心,并kalman滤波

--- src/dynamic_detection/src/dynamic.cpp
+++ src/dynamic_detection/src/dynamic.cpp
@@ -5,6 +5,7 @@
 #include <map_msgs/ProjectedMap.h>
 #include <nav_msgs/OccupancyGrid.h>
 #include <nav_msgs/Odometry.h>
+#include <nav_msgs/Path.h>
 #include <geometry_msgs/Pose.h>
 #include <unistd.h>
 #include <tf/transform_broadcaster.h>
@@ -23,6 +24,21 @@
 #define SOLUTION 0.05
 #define map_x 200
 #define map_y 200
+typedef struct KAL_PARAM
+{
+    float C_last;
+    float X_last;
+
+    float Q;
+    float R;
+
+    float K;
+    float X;
+    float C;
+
+    float input;
+}
+kal_param;
 
 /********************************话题订阅回调函数声明*****************************************/
 /*************************************************************************************/
@@ -37,14 +53,21 @@ void cal_centroid(std::queue<int> group_x,std::queue<int> group_y,int *centroid)
 void pthread_publish(void);
 std::queue<int> beam_substract(float *minuend,float * subtrahend,int n );//激光束对应的beam作差,如果超过阈值,则认为有动态物体.
 void map_update(int **map,float *beams, std::queue<int> mark_beam,float AglRbt_m);//地图更新函数,根据mark_beam来跟新动态物体的位置
-void kalman_vel(int *coordinate);//kalman filter
+float kalman_filter(kal_param *k_flt,int input);//kalman filter
 
 /********************************全局变量声明*****************************************/
 /*************************************************************************************/
+//x,y卡尔曼滤波的参数
+kal_param x_pos_param={0,0,1,1,0,0,0,0};
+kal_param y_pos_param;
+kal_param x_vel_param;
+kal_param y_vel_param;
+
 time_t tm;
 float scan_data[11][1081]={{0}};      //一共保存11帧数据,每组数据有1081个beams.
 static int frames=0;            //静态数据
 nav_msgs::OccupancyGrid Map_Msg;//栅格地图消息
+//nav_msgs::Path Velocity_Msg;
 geometry_msgs::Pose RobotCenter;//地图中心,应该定义为机器人的中心
 int map[map_x][map_y];          //地图数组
 float AngleIncrement;
@@ -56,14 +79,17 @@ int main(int argc, char *argv[])
     ros::NodeHandle nh;
     ROS_INFO("***********START*************");
 
-    ros::Publisher pub=nh.advertise<nav_msgs::OccupancyGrid>("/substracted",1000);
-    ros::Subscriber sub=nh.subscribe("/base_scan",1000,&data_substract);
-    ros::Subscriber robot_pose=nh.subscribe("/odom",1000,&rbtPose);
+    ros::Publisher pub_map=nh.advertise<nav_msgs::OccupancyGrid>("/substracted",1000);  //publish the local map of dynamics
+//    ros::Publisher pub_velocity=nh.advertise<nav_msgs::Path >("/kalman_filter",1000);      //publish the velocity of dynamics
+    ros::Subscriber sub=nh.subscribe("/base_scan",1000,&data_substract);                                  //subscribe the laser data
+    ros::Subscriber robot_pose=nh.subscribe("/odom",1000,&rbtPose);                                        //subscribe the odometry of robot
+//    Velocity_Msg.poses.data();
 
     ros::Rate rate(5);
     while(ros::ok())
     {
-        pub.publish(Map_Msg);
+        pub_map.publish(Map_Msg);
+//        pub_velocity.publish(Velocity_Msg);
         ROS_INFO("PUBLISH...");
         ros::spinOnce();
         rate.sleep();
@@ -94,8 +120,8 @@ void data_substract(const sensor_msgs::LaserScan& msg)
     }
     if(!mark_beam.empty())
     {
-        ROS_INFO("MARKED_BEAM: %d",mark_beam.front());
-        mark_beam.pop();
+        //ROS_INFO("MARKED_BEAM: %d",mark_beam.front());
+        //mark_beam.pop();
         /*调用地图更新函数*/
         map_update((int **)map,*(scan_data+frames),(std::queue<int>)mark_beam,AglRbt);
     }
@@ -137,7 +163,7 @@ std::queue<int> beam_substract(float *minuend,float * subtrahend,int n )
     for(int i=0;i<n;i++)
     {
       result=  minuend[i]-subtrahend[i];
-      if(result>BEAM_THRESH || result<-BEAM_THRESH  )
+      if((result>BEAM_THRESH || result<-BEAM_THRESH) && result<BEAM_THRESH+2 && result>-2-BEAM_THRESH)//要求运动物体运动一秒的距离大于临界值,但又不允许其跳变
       {
           marked_beam.push(i);
       }
@@ -146,15 +172,19 @@ std::queue<int> beam_substract(float *minuend,float * subtrahend,int n )
 }
 
 //地图更新函数
+/*@des 地图更新函数,只是将所有的拟动态物体标示出来.并未加以区分,应该用区域增长法将各动态物体区分开来
+ * @map 地图指针,@beam 激光帧, @mark_beam 被标记激光的队列,@AglRbt_m
+ **/
 void map_update(int **map,float *beams, std::queue<int> mark_beam,float AglRbt_m)
 {
     int x_grid;
     int y_grid;
     float theta=0;
     float range=0;
+    float last_average_range;
     std::queue<int> x_group;
     std::queue<int> y_group;
-    int ctrd[2]={0,0};
+    int ctrd[2]={0,0};    //ctrd[0]是中心点x的坐标..ctrd[1]是中心点y的坐标
 
     while(!mark_beam.empty())
     {
@@ -169,18 +199,23 @@ void map_update(int **map,float *beams, std::queue<int> mark_beam,float AglRbt_m
 //        }
         range=beams[mark_beam.front()];
         mark_beam.pop();
-//        if(range>5) continue;//雷达边界附近的激光舍去 ...this code will cause running error :Floating point exception  (core dumped)
-
-        x_grid=(range*cos(theta)/SOLUTION);//变动激光击中的点的x坐标
+        if(range>MAX_RANGE-0.2) continue;//雷达边界附近的激光舍去
+		x_grid=(range*cos(theta)/SOLUTION);//变动激光击中的点的x坐标
         y_grid=-(range*sin(theta)/SOLUTION);//变动激光击中的点的y坐标
 
         x_group.push(x_grid);
         y_group.push(y_grid);
 
-        //*((int *)map +(map_x*map_y/2+map_y/2)+ (x_grid-1)*map_y + y_grid)=100;
+//        *((int *)map +(map_x*map_y/2+map_y/2)+ (x_grid-1)*map_y + y_grid)=100;
+
     }
     cal_centroid(x_group,y_group,ctrd);
-    *((int *)map +(map_x*map_y/2+map_y/2)+ (ctrd[0]-1)*map_y + ctrd[1])=100;
+
+//    last_average_range=SOLUTION*sqrt(ctrd[0]^2+ctrd[1]^2);
+//	ctrd[0]=kalman_filter(&x_pos_param, ctrd[0]);
+//	ctrd[1]=kalman_filter(&y_pos_param, ctrd[1]);
+
+    *((int *)map + (map_x*map_y/2+map_y/2) + (ctrd[0]-1)*map_y + ctrd[1])=100;
 }
 
 /*@des 计算质心函数
@@ -193,20 +228,35 @@ void cal_centroid(std::queue<int> group_x,std::queue<int> group_y,int *centroid)
     num=group_x.size();
     while(!group_x.empty())
     {
+
         sum_x += group_x.front();
         sum_y += group_y.front();
 
         group_x.pop();
         group_y.pop();
     }
-    centroid[0] = sum_x/num;
-    centroid[1] = sum_y/num;
+	if(num!=0)
+	{
+		centroid[0] = (int) (sum_x/num);
+	    centroid[1] = (int) (sum_y/num);
+	}
+
 }
 /*@des
  * @
  * @
  * @
  * */
-void kalman_vel(int *coordinate)
+float kalman_filter(kal_param *k_flt,int input)
 {
+    //C表示最优值协方差,C_last表示转移预测方差.X表示最优数据.Q表示转移噪声方差.R为测量噪声方差,K表示卡氏增益
+    k_flt->input=input;
+    k_flt->K = (k_flt->C_last)/(k_flt->C_last+k_flt->R);
+    k_flt->X = k_flt->X_last + k_flt->K *(k_flt->input - k_flt->X_last);//此处的转移模型认为 两次测量之间值是不变的.对与速度预测是合理的,但是对于位置的预测是不合理的,应该是加上速度乘以时间间隔.
+    k_flt->C = (1 - k_flt->K) * (k_flt->C_last);
+
+    k_flt->X_last = k_flt->X;
+    k_flt->C_last = k_flt->C + k_flt->Q;
+
+    return k_flt->X;
 }
